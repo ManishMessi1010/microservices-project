80e1e0531da12d6e452fc7244502fa29bf84a4da
Manish Patil <manishpatil11482@gmail.com>
2026-08-28 15:55:22 +0530

    local checking done, added Dockerfiles now pushing

---
+++ order-service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/orders')
+def orders():
+    return jsonify({
+        "orders": [
+            "order-101",
+            "order-102",
+            "order-103"
+        ]
+    })
+
+
+if __name__ == '__main__':
+    app.run(port=5003, debug=True)
